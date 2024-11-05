93346437df3cc323a4efbb921073dd9180b361fa
matheus98g <gollmannmatheus@gmail.com>
2024-11-05 20:08:45 -0300

    cadastro usuario - hash password working

--- controller/newUserController.php
+++ controller/newUserController.php
@@ -5,11 +5,14 @@ include('../model/db.php');
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$password_hash = password_hash($password, PASSWORD_BCRYPT);
 $turma = $_POST['turma'];
 
 
-$query = "INSERT INTO usuario (nomeUsuario, emailUsuario, senhaUsuario, turmaUsuario, dataCadastro) VALUES ('$nome', '$email', '$password', '$turma', now());";
+$query = "INSERT INTO usuario (nomeUsuario, emailUsuario, senhaUsuario, turmaUsuario, dataCadastro) VALUES ('$nome', '$email', '$password_hash', '$turma', now());";
 
 $result = mysqli_query($db, $query) or die(false);
 
-echo "$nome <br> $email <br> $password <br> $turma";
+if ($result) {
+    echo "Usuario cadastrado com sucesso!";
+}
